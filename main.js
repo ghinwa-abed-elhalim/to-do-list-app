a8f1e09090a77e1a0e79da6cfcf3e02da420b075
Ghinwa Abed Elhalim <ghenwaah77@gmail.com>
2025-04-16 22:26:17 +0100

    variables added

---
+++ main.js
@@ -0,0 +1,7 @@
+let all_tasks = [];
+let current_filter = 'All';
+
+const input_field = document.querySelector('.task-input')
+const add_button = document.querySelector('.add-task-btn')
+const list_container = document.querySelector('task-list')
+const filter_buttons = document.querySelectorAll('.filter-btn')
