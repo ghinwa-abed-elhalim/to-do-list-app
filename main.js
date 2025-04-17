c004036d0e91baa0c13a7022958f75e3193973c5
Ghinwa Abed Elhalim <ghenwaah77@gmail.com>
2025-04-17 01:07:55 +0100

    event listener added

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ const input_field = document.querySelector('.task-input')
 const add_button = document.querySelector('.add-task-btn')
 const list_container = document.querySelector('task-list')
 const filter_buttons = document.querySelectorAll('.filter-btn')
+
+if (add_button) {
+    add_button.addEventListener('click', add_task)
+}
+if (filter_buttons.length > 0) {
+    filter_buttons.forEach(function (button) {
+        button.addEventListener('click', function (){
+            current_filter = button.innerText;
+            show_tasks()
+        });
+    });
+}
